92931064aed21faf5d2945edd00ec0602af78a2f
Sergey Kutsenko <sergey.wild.man@gmail.com>
2026-09-07 17:59:55 +0400

    - test redis writes

--- src/stream.rs
+++ src/stream.rs
@@ -3,23 +3,43 @@ use skw_lib_http_protos::{
     ping::{RpcMethod, RpcPayload},
 };
 use skw_lib_shared::prelude::{
+    APP,
     chrono::*,
     iggy::{RpcRequestMeta, run_service_consumer},
     jsonrpc::ServiceHttpError,
     log::*,
+    redis::{AsyncCommands, ConnectionManager},
 };
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    run_service_consumer::<JsonRpcServiceRequest<RpcMethod>, _, _, _>(handle_message, None, Some(100)).await?;
+    let redis = APP.redis.clone().connect().await;
+    let redis_conn = redis.inner().expect("bad redis connection");
+
+    run_service_consumer::<JsonRpcServiceRequest<RpcMethod>, _, _, _>(
+        move |rpc_method, meta| handle_message(rpc_method, meta, redis_conn.clone()),
+        None,
+        Some(100),
+    )
+    .await?;
     Ok(())
 }
 
-async fn handle_message(rpc_method: RpcMethod, _meta: RpcRequestMeta) -> Result<RpcPayload, ServiceHttpError> {
+async fn handle_message(rpc_method: RpcMethod, _meta: RpcRequestMeta, mut redis: ConnectionManager) -> Result<RpcPayload, ServiceHttpError> {
     debug!("{:?}", rpc_method);
     debug!("signature:{:?}", _meta);
     match rpc_method {
         RpcMethod::Public {} => Ok(RpcPayload::Public {}),
-        RpcMethod::Ping {} => Ok(RpcPayload::Ping { ts: Utc::now() }),
+        RpcMethod::Ping {} => {
+            let count: u64 = redis.incr("ping:count", 1).await.map_err(|e| {
+                error!("redis incr failed: {e}");
+                ServiceHttpError::InternalServerError
+            })?;
+
+            Ok(RpcPayload::Ping {
+                ts: Utc::now(),
+                count,
+            })
+        }
     }
 }
